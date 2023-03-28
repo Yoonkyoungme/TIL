306c39b77b90d299f6d8aa7f062cd4d4f13ec5a3
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-03-28 22:16:28 +0900

    feat: delete to-do list

--- react/projects/to-do/src/App.js
+++ react/projects/to-do/src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false,
+      },
+    ],
+  };
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -11,25 +26,18 @@ export default class App extends Component {
     float: "right",
   };
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false,
-    },
-  ];
-
   getStyle = () => {
     return {
       padding: "10px",
       borderBottom: "1px dotted #ccc",
       TextDecoration: "none",
     };
+  };
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    console.log("newTodoData", newTodoData);
+    this.setState({ todoData: newTodoData });
   };
 
   render() {
@@ -39,10 +47,16 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={false} /> {data.title}
-              <button style={this.btnStyle}>x</button>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleClick(data.id)}
+              >
+                x
+              </button>
             </div>
           ))}
         </div>
