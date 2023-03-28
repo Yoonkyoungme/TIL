dabf6bd8e93b2282a9048c0cca6b291bc1cce80e
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-03-28 22:39:59 +0900

    feat: input and add new to-do list

--- react/projects/to-do/src/App.js
+++ react/projects/to-do/src/App.js
@@ -15,6 +15,7 @@ export default class App extends Component {
         completed: false,
       },
     ],
+    value: "",
   };
 
   btnStyle = {
@@ -40,6 +41,24 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData });
   };
 
+  handleChange = (e) => {
+    console.log("e", e.target.value);
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  };
+
   render() {
     return (
       <div className="container">
@@ -59,6 +78,23 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
         </div>
       </div>
     );
