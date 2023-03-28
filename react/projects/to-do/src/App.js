acd7903caf898740c596fccda4f88ee40ffeeea1
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-03-28 15:02:44 +0900

    feat: add to-do lists use map method

--- react/projects/to-do/src/App.js
+++ react/projects/to-do/src/App.js
@@ -11,6 +11,19 @@ export default class App extends Component {
     float: "right",
   };
 
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+
   getStyle = () => {
     return {
       padding: "10px",
@@ -26,10 +39,12 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          <div style={this.getStyle()}>
-            <input type="checkbox" defaultChecked={false} /> 공부하기
-            <button style={this.btnStyle}>x</button>
-          </div>
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} /> {data.title}
+              <button style={this.btnStyle}>x</button>
+            </div>
+          ))}
         </div>
       </div>
     );
