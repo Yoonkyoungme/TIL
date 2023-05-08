35b94b51dfd00d72c3c64b25faacdde7ee7ad8d5
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-05-09 01:24:05 +0900

    Feat: add padZero function for formatting single digit numbers

--- JavaScript/projects/clock/script.js
+++ JavaScript/projects/clock/script.js
@@ -3,15 +3,21 @@ const timeDiv = document.getElementById("time");
 
 function getTime() {
   let now = new Date();
-  let year = now.getFullYear();
-  let month = now.getMonth() + 1;
-  let date = now.getDate();
-  let hour = now.getHours();
-  let minute = now.getMinutes();
-  let second = now.getSeconds();
+  let year = padZero(now.getFullYear());
+  let month = padZero(now.getMonth() + 1);
+  let date = padZero(now.getDate());
+  let hour = padZero(now.getHours());
+  let minute = padZero(now.getMinutes());
+  let second = padZero(now.getSeconds());
 
   todayDiv.textContent = `${year}년 ${month}월 ${date}일`;
   timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
+
+  setInterval(getTime, 1000);
 }
 
-setInterval(getTime, 1000);
+function padZero(data) {
+  return data < 10 ? `0${data}` : data;
+}
+
+getTime();
