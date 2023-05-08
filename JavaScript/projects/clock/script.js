4a054d3150811087efbf90bbef69ad801086bdbb
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-05-08 18:27:10 +0900

    Feat: add today data

--- JavaScript/projects/clock/script.js
+++ JavaScript/projects/clock/script.js
@@ -1,12 +1,16 @@
+const todayDiv = document.getElementById("today");
 const timeDiv = document.getElementById("time");
 
 function getTime() {
   let now = new Date();
-
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let date = now.getDate();
   let hour = now.getHours();
   let minute = now.getMinutes();
   let second = now.getSeconds();
 
+  todayDiv.textContent = `${year}년 ${month}월 ${date}일`;
   timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
 }
 
